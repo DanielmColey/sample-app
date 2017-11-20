1b8d85fcc3150fe47e4f8b6d4a92aaf7e143ed26
DanielmColey <dmcoley948562@gmail.com>
2017-11-20 14:29:50 -0700

    Added microposts with image uploader

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.create!(name:  "Dan Coley",
              activated: true,
              activated_at: Time.zone.now)
 
-151.times do |n|
+70.times do |n|
   name = Faker::Pokemon.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -16,4 +16,10 @@ User.create!(name:  "Dan Coley",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
